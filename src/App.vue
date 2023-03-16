ed43b54307d7c1f0a43d19d75c5030f02c44eef5
Felipe dos Santos Oliveira <felipedossantosads@gmail.com>
2023-03-16 09:28:04 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import RenderIcon from "./components/RenderIcon.vue";
+
+const icons = ["CubeIcon", "BeakerIcon", "BugAntIcon", "GlobeAmericasIcon"];
+</script>
+
+<template>
+  <div class="bg-slate-100 h-[720px]">
+    <header class="p-10 w-full flex flex-col">
+      <h1 class="font-bold text-xl text-center">Dynamic Load Icons</h1>
+      <h2 class="font-semibold underline text-lg text-center">
+        <a href="https://heroicons.com/"> Heroicons Library </a>
+      </h2>
+    </header>
+    <div class="px-10">
+      <RenderIcon />
+    </div>
+  </div>
+</template>
---
+++ src/components/RenderIcon.vue
@@ -0,0 +1,29 @@
+<script setup>
+import * as heroicon from "@heroicons/vue/24/outline";
+import ProcessingIcon from "./ProcessingIcon.vue";
+import { reactive } from "vue";
+
+const icon = reactive({
+  data: "BugAntIcon",
+});
+</script>
+<template>
+  <div class="flex space-x-4 items-end">
+    <div class="flex-1">
+      <label for="icon" class="font-semibold">Icon name:</label>
+      <input
+        type="text"
+        v-model="icon.data"
+        class="block p-4 w-full rounded-md border-2 bg-violet-50 border-violet-300 shadow-lg focus:border-sky-500 focus:ring-sky-500"
+      />
+    </div>
+    <div class="flex flex-none items-center justify-center">
+      <div class="p-3 bg-violet-50 border-2 rounded-md border-violet-300">
+        <component
+          :is="heroicon[icon.data] ? heroicon[icon.data] : ProcessingIcon"
+          class="w-8"
+        />
+      </div>
+    </div>
+  </div>
+</template>
